0cf01bfc19a89a16e5a1f31f2cb736a91c59ab08
Daniel Rosas Nunez <darosas-@c2r9s2.42malaga.com>
2025-06-03 21:41:22 +0200

    starting minitalk

---
+++ include/minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: darosas- <darosas-@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/03 18:08:36 by darosas-          #+#    #+#             */
+/*   Updated: 2025/06/03 19:44:52 by darosas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+# include "ft_printf.h"
+
+
+
+#endif
---
+++ makefile
@@ -0,0 +1,46 @@
+SERVER = server
+CLIENT = client
+
+COMPILER = cc
+
+FLAGS = -Wall -Werror -Wextra
+
+INCLUDES = -I include -I libft/includes
+
+LIBFT_PATH = libft
+LIBFT = $(LIBFT_PATH)/libft.a
+
+SRC_PATH = src
+
+SRC_S = $(SRC_PATH)/server.c
+OBJECTS_S = $(SRC_S:%.c=%.o)
+
+SRC_C = $(SRC_PATH)/client.c
+OBJECTS_C = $(SRC_C:%.c=%.o)
+
+all: libft $(SERVER)
+
+libft:
+	@if [ ! -f "$(LIBFT)" ]; then \
+		make -s -C $(LIBFT_PATH); \
+	fi
+
+$(SERVER): $(OBJECTS_S)
+	$(COMPILER) $(OBJECTS_S) $(LIBFT) -o $(SERVER)
+
+%.o: %.c
+	$(COMPILER) $(FLAGS) $(INCLUDES) -c $< -o $@
+
+clean:
+	@rm -r $(OBJECTS_S)
+	@make clean -s -C $(LIBFT_PATH)
+	@echo "clean done!"
+
+fclean: clean
+	@rm -r $(SERVER)
+	@make fclean -s -C $(LIBFT_PATH)
+	@echo "fclean done!"
+
+re: fclean all
+
+.PHONY: all libft clean fclean re
